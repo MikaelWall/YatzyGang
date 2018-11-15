3776a60de1f1e956162c06152a422719b1da21af
Rikard Jalex <*rikardjalex@gmail.com*>
2018-11-15 15:59:14 +0100

    Test dice roll. kan rulla men inte spara chkbox

--- diceRoller.js
+++ diceRoller.js
@@ -10,6 +10,22 @@ function printNumber(number) {
     var placeholder = document.getElementById('placeholder');
     placeholder.innerHTML = number;
 }
+function printNumber2(number) {
+    var placeholder2 = document.getElementById('placeholder2');
+    placeholder2.innerHTML = number;
+}
+function printNumber3(number) {
+    var placeholder3 = document.getElementById('placeholder3');
+    placeholder3.innerHTML = number;
+}
+function printNumber4(number) {
+    var placeholder4 = document.getElementById('placeholder4');
+    placeholder4.innerHTML = number;
+}
+function printNumber5(number) {
+    var placeholder5 = document.getElementById('placeholder5');
+    placeholder5.innerHTML = number;
+}
 
 
 document.addEventListener("DOMContentLoaded", function(e){
@@ -19,11 +35,14 @@ document.addEventListener("DOMContentLoaded", function(e){
     button.onclick = function () {
         var result = dice.roll();
         printNumber(result);
+        var result2 = dice.roll();
+        printNumber2(result2);
+        var result3 = dice.roll();
+        printNumber3(result3);
+        var result4 = dice.roll();
+        printNumber4(result4);
+        var result5 = dice.roll();
+        printNumber5(result5);
     };
 });
 
-var valueP1_1 = document.getElementById("player1_ones"); //skapa variabel som tar ut värdet från P1 ettor
-valueP1_1.addEventListener("change", updateValue); //händer när eventet "change" alltså när vi ändrar i inputen
-function updateValue() {
-    document.getElementById("totalvalue").totalvalue += valueP1_1; 
-} // funktionen som adderar allting
