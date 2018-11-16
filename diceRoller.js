b4bd47c6892d28c66d75aaf43c80c5b893e52cd8
Mikael7Wall <mikael.7.wall@gmail.com>
2018-11-16 15:59:56 +0100

    checkbox klar

--- diceRoller.js
+++ diceRoller.js
@@ -6,43 +6,62 @@ var dice = {
     }
 }
 
+var checkbox1 = document.getElementById('box1');
+
 function printNumber(number) {
     var placeholder = document.getElementById('placeholder');
     placeholder.innerHTML = number;
 }
+
 function printNumber2(number) {
     var placeholder2 = document.getElementById('placeholder2');
     placeholder2.innerHTML = number;
 }
+
 function printNumber3(number) {
     var placeholder3 = document.getElementById('placeholder3');
     placeholder3.innerHTML = number;
 }
+
 function printNumber4(number) {
     var placeholder4 = document.getElementById('placeholder4');
     placeholder4.innerHTML = number;
 }
+
 function printNumber5(number) {
     var placeholder5 = document.getElementById('placeholder5');
     placeholder5.innerHTML = number;
 }
 
 
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
     var button = document.getElementById('button');
     console.log(button);
 
     button.onclick = function () {
-        var result = dice.roll();
-        printNumber(result);
-        var result2 = dice.roll();
-        printNumber2(result2);
-        var result3 = dice.roll();
-        printNumber3(result3);
-        var result4 = dice.roll();
-        printNumber4(result4);
-        var result5 = dice.roll();
-        printNumber5(result5);
+        if (document.getElementById("box1").checked == false) {
+            var result = dice.roll();
+            printNumber(result);
+        }
+
+        if (document.getElementById("box2").checked == false) {
+            var result2 = dice.roll();
+            printNumber2(result2);
+        }
+
+        if (document.getElementById("box3").checked == false) {
+            var result3 = dice.roll();
+            printNumber3(result3);
+        }
+
+        if (document.getElementById("box4").checked == false) {
+            var result4 = dice.roll();
+            printNumber4(result4);
+        }
+
+        if (document.getElementById("box5").checked == false) {
+            var result5 = dice.roll();
+            printNumber5(result5);
+        }
     };
 });
-
